8623dcf14d32ce1a8d6c96a87c2e4c2dd9e6f91e
Alex Qiu <alexq@nvidia.com>
2025-08-13 23:51:51 +0800

    fix

--- recipe/async_dapo/dapo_ray_trainer.py
+++ recipe/async_dapo/dapo_ray_trainer.py
@@ -343,8 +343,8 @@ class RayDAPOTrainer(RayPPOTrainer):
 
         prev_step_profile = False
         curr_step_profile = (
-            self.global_steps in self.config.global_profiler.steps
-            if self.config.global_profiler.steps is not None
+            self.global_steps in self.config.trainer.steps
+            if self.config.trainer.steps is not None
             else False
         )
         next_step_profile = False
@@ -360,7 +360,7 @@ class RayDAPOTrainer(RayPPOTrainer):
                 with marked_timer("start_profile", timing_raw):
                     self._start_profiling(
                         not prev_step_profile and curr_step_profile
-                        if self.config.global_profiler.profile_continuous_steps
+                        if self.config.trainer.profile_continuous_steps
                         else curr_step_profile
                     )
 
@@ -544,13 +544,13 @@ class RayDAPOTrainer(RayPPOTrainer):
 
                 with marked_timer("stop_profile", timing_raw):
                     next_step_profile = (
-                        self.global_steps + 1 in self.config.global_profiler.steps
-                        if self.config.global_profiler.steps is not None
+                        self.global_steps + 1 in self.config.trainer.steps
+                        if self.config.trainer.steps is not None
                         else False
                     )
                     self._stop_profiling(
                         curr_step_profile and not next_step_profile
-                        if self.config.global_profiler.profile_continuous_steps
+                        if self.config.trainer.profile_continuous_steps
                         else curr_step_profile
                     )
                     prev_step_profile = curr_step_profile
