e01376663b47107667b58eca9c353032b4dc1dfd
jiaqiw09 <60021713+jiaqiw09@users.noreply.github.com>
2025-10-10 21:54:27 +0800

    [megatron] feat: add ascend megatron merge support (#3722)
    ### What does this PR do?
    
    add ascend megatron merge support
    
    ### Checklist Before Starting
    
    - [x] Search for similar PRs. Paste at least one query link here: ...
    - [x] Format the PR title as `[{modules}] {type}: {description}` (This
    will be checked by the CI)
    - `{modules}` include `fsdp`, `megatron`, `sglang`, `vllm`, `rollout`,
    `trainer`, `ci`, `training_utils`, `recipe`, `hardware`, `deployment`,
    `ray`, `worker`, `single_controller`, `misc`, `perf`, `model`, `algo`,
    `env`, `tool`, `ckpt`, `doc`, `data`
    - If this PR involves multiple modules, separate them with `,` like
    `[megatron, fsdp, doc]`
      - `{type}` is in `feat`, `fix`, `refactor`, `chore`, `test`
    - If this PR breaks any API (CLI arguments, config, function signature,
    etc.), add `[BREAKING]` to the beginning of the title.
      - Example: `[BREAKING][fsdp, megatron] feat: dynamic batching`
    
    ### Test
    
    > For changes that can not be tested by CI (e.g., algorithm
    implementation, new model support), validate by experiment(s) and show
    results like training curve plots, evaluation results, etc.
    
    ### API and Usage Example
    
    > Demonstrate how the API changes if any, and provide usage example(s)
    if possible.
    
    ```python
    # Add code snippet or script demonstrating how to use this
    ```
    
    ### Design & Code Changes
    
    > Demonstrate the high-level design if this PR is complex, and list the
    specific changes.
    
    ### Checklist Before Submitting
    
    > [!IMPORTANT]
    > Please check all the following items before requesting a review,
    otherwise the reviewer might deprioritize this PR for review.
    
    - [x] Read the [Contribute
    Guide](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md).
    - [x] Apply [pre-commit
    checks](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md#code-linting-and-formatting):
    `pre-commit install && pre-commit run --all-files --show-diff-on-failure
    --color=always`
    - [ ] Add / Update [the
    documentation](https://github.com/volcengine/verl/tree/main/docs).
    - [ ] Add unit or end-to-end test(s) to [the CI
    workflow](https://github.com/volcengine/verl/tree/main/.github/workflows)
    to cover all the code. If not feasible, explain why: ...
    - [ ] Once your PR is ready for CI, send a message in [the `ci-request`
    channel](https://verl-project.slack.com/archives/C091TCESWB1) in [the
    `verl` Slack
    workspace](https://join.slack.com/t/verl-project/shared_invite/zt-3855yhg8g-CTkqXu~hKojPCmo7k_yXTQ).
    (If not accessible, please try [the Feishu group
    (飞书群)](https://applink.larkoffice.com/client/chat/chatter/add_by_link?link_token=772jd4f1-cd91-441e-a820-498c6614126a).)

--- verl/model_merger/megatron_model_merger.py
+++ verl/model_merger/megatron_model_merger.py
@@ -22,6 +22,13 @@ from typing import Any, Callable, ContextManager
 import numpy as np
 import torch
 import torch.distributed as dist
+
+try:
+    # NPU patch
+    import mindspeed.megatron_adaptor  # noqa: F401
+except ImportError:
+    pass
+
 from accelerate import init_empty_weights
 from megatron.core import mpu
 from megatron.core.models.gpt.gpt_model import ModelType
