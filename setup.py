ab4c04b0cdadfe76fd3b817f7db5e4bd33ac2e15
ℍ𝕠𝕝𝕝𝕠𝕨 𝕄𝕒𝕟 <hollowman@opensuse.org>
2025-10-25 07:24:49 +0300

    [doc] chore: update installation scripts to use newer versions (#3901)
    ### What does this PR do?
    
    > Add **concise** overview of what this PR aims to achieve or
    accomplish. Reference related GitHub issues and PRs that help with the
    review.
    
    - Fix typos in the verl image README: `nvidia-cudnn-cu12`
    - Upgrade to use mcore >= 0.13.0 since the fused forward patch now
    requires that
    https://github.com/volcengine/verl/pull/3849#issuecomment-3432115063
    - Upgrade vllm to the latest, sglang to the version indicated in
    `setup.py`, which means now we will use Torch 2.8, CUDA 12.8, cuDNN 9.10
    and flashinfer 0.3.1.
    - Use flash-attn 2.8.1 since that's the oldest version that has Torch
    2.8 precompiled support, also it's the latest flash-attn version
    supported by TransformerEngine (starting from 2.6 till latest 2.8)
    - Fix the documentation since apex should not be needed in FSDP backend.
    - Add note mentioning that pyext is lack of maintainace and cannot work
    with python 3.12, and they can install from a separate fork when.
    
    ### Checklist Before Starting
    
    - [X] Search for similar PRs. Paste at least one query link here: ...
    - [X] Format the PR title as `[{modules}] {type}: {description}` (This
    will be checked by the CI)
    - `{modules}` include `fsdp`, `megatron`, `sglang`, `vllm`, `rollout`,
    `trainer`, `ci`, `training_utils`, `recipe`, `hardware`, `deployment`,
    `ray`, `worker`, `single_controller`, `misc`, `perf`, `model`, `algo`,
    `env`, `tool`, `ckpt`, `doc`, `data`
    - If this PR involves multiple modules, separate them with `,` like
    `[megatron, fsdp, doc]`
      - `{type}` is in `feat`, `fix`, `refactor`, `chore`, `test`
    - If this PR breaks any API (CLI arguments, config, function signature,
    etc.), add `[BREAKING]` to the beginning of the title.
      - Example: `[BREAKING][fsdp, megatron] feat: dynamic batching`
    
    ### Test
    
    > For changes that can not be tested by CI (e.g., algorithm
    implementation, new model support), validate by experiment(s) and show
    results like training curve plots, evaluation results, etc.
    
    I installed the environments in this way and it works fine.
    
    ### API and Usage Example
    
    > Demonstrate how the API changes if any, and provide usage example(s)
    if possible.
    
    ```python
    # Add code snippet or script demonstrating how to use this
    ```
    
    ### Design & Code Changes
    
    > Demonstrate the high-level design if this PR is complex, and list the
    specific changes.
    
    ### Checklist Before Submitting
    
    > [!IMPORTANT]
    > Please check all the following items before requesting a review,
    otherwise the reviewer might deprioritize this PR for review.
    
    - [X] Read the [Contribute
    Guide](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md).
    - [X] Apply [pre-commit
    checks](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md#code-linting-and-formatting):
    `pre-commit install && pre-commit run --all-files --show-diff-on-failure
    --color=always`
    - [X] Add / Update [the
    documentation](https://github.com/volcengine/verl/tree/main/docs).
    - [X] Add unit or end-to-end test(s) to [the CI
    workflow](https://github.com/volcengine/verl/tree/main/.github/workflows)
    to cover all the code. If not feasible, explain why: ...
    - [X] Once your PR is ready for CI, send a message in [the `ci-request`
    channel](https://verl-project.slack.com/archives/C091TCESWB1) in [the
    `verl` Slack
    workspace](https://join.slack.com/t/verl-project/shared_invite/zt-3855yhg8g-CTkqXu~hKojPCmo7k_yXTQ).
    (If not accessible, please try [the Feishu group
    (飞书群)](https://applink.larkoffice.com/client/chat/chatter/add_by_link?link_token=772jd4f1-cd91-441e-a820-498c6614126a).)
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ PRIME_REQUIRES = ["pyext"]
 GEO_REQUIRES = ["mathruler", "torchvision", "qwen_vl_utils"]
 GPU_REQUIRES = ["liger-kernel", "flash-attn"]
 MATH_REQUIRES = ["math-verify"]  # Add math-verify as an optional dependency
-VLLM_REQUIRES = ["tensordict>=0.8.0,<=0.10.0,!=0.9.0", "vllm>=0.7.3,<=0.9.1"]
+VLLM_REQUIRES = ["tensordict>=0.8.0,<=0.10.0,!=0.9.0", "vllm>=0.8.5,<=0.11.0"]
 SGLANG_REQUIRES = [
     "tensordict>=0.8.0,<=0.10.0,!=0.9.0",
     "sglang[srt,openai]==0.5.2",
