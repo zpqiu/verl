aa16e55fe17aff4d8b844c696ba16d6514095eb6
sophiayyya <yangjingyi0730@163.com>
2026-02-03 10:39:42 +0800

    [data] fix: keyword video_metadata (#5177)
    ### What does this PR do?
    
    This PR is to fix the bug of wrong keyword for video meta data.
    Current code `video_metadatas` is a typo and inconsistent with
    `transformers` processor code.
    E.g.
    
    https://github.com/huggingface/transformers/blob/0fa2c2f06f91129b89462e0136c0a25618e1a31b/src/transformers/models/qwen3_vl/processing_qwen3_vl.py#L116
    
    https://github.com/huggingface/transformers/blob/0fa2c2f06f91129b89462e0136c0a25618e1a31b/src/transformers/models/qwen2_5_vl/processing_qwen2_5_vl.py#L102
    
    https://github.com/huggingface/transformers/blob/0fa2c2f06f91129b89462e0136c0a25618e1a31b/src/transformers/models/glm4v/processing_glm4v.py#L99
    
    ### Checklist Before Starting
    
    - [x] Search for similar PRs. Paste at least one query link here: ...
    - [x] Format the PR title as `[{modules}] {type}: {description}` (This
    will be checked by the CI)
    - `{modules}` include `fsdp`, `megatron`, `veomni`, `sglang`, `vllm`,
    `rollout`, `trainer`, `ci`, `training_utils`, `recipe`, `hardware`,
    `deployment`, `ray`, `worker`, `single_controller`, `misc`, `perf`,
    `model`, `algo`, `env`, `tool`, `ckpt`, `doc`, `data`, `cfg`, `reward`
    - If this PR involves multiple modules, separate them with `,` like
    `[megatron, fsdp, doc]`
      - `{type}` is in `feat`, `fix`, `refactor`, `chore`, `test`
    - If this PR breaks any API (CLI arguments, config, function signature,
    etc.), add `[BREAKING]` to the beginning of the title.
      - Example: `[BREAKING][fsdp, megatron] feat: dynamic batching`
    
    ### Test
    
    > For changes that can not be tested by CI (e.g., algorithm
    implementation, new model support), validate by experiment(s) and show
    results like training curve plots, evaluation results, etc.
    
    ### API and Usage Example
    
    > Demonstrate how the API changes if any, and provide usage example(s)
    if possible.
    
    ```python
    # Add code snippet or script demonstrating how to use this
    ```
    
    ### Design & Code Changes
    
    > Demonstrate the high-level design if this PR is complex, and list the
    specific changes.
    
    ### Checklist Before Submitting
    
    > [!IMPORTANT]
    > Please check all the following items before requesting a review,
    otherwise the reviewer might deprioritize this PR for review.
    
    - [x] Read the [Contribute
    Guide](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md).
    - [ ] Apply [pre-commit
    checks](https://github.com/volcengine/verl/blob/main/CONTRIBUTING.md#code-linting-and-formatting):
    `pre-commit install && pre-commit run --all-files --show-diff-on-failure
    --color=always`
    - [ ] Add / Update [the
    documentation](https://github.com/volcengine/verl/tree/main/docs).
    - [ ] Add unit or end-to-end test(s) to [the CI
    workflow](https://github.com/volcengine/verl/tree/main/.github/workflows)
    to cover all the code. If not feasible, explain why: ...
    - [ ] Once your PR is ready for CI, send a message in [the `ci-request`
    channel](https://verl-project.slack.com/archives/C091TCESWB1) in [the
    `verl` Slack
    workspace](https://join.slack.com/t/verl-project/shared_invite/zt-3855yhg8g-CTkqXu~hKojPCmo7k_yXTQ).
    (If not accessible, please try [the Feishu group
    (飞书群)](https://applink.larkoffice.com/client/chat/chatter/add_by_link?link_token=772jd4f1-cd91-441e-a820-498c6614126a).)
    - [ ] If your PR is related to the `recipe` submodule, please also
    update the reference to the submodule commit via `git submodule update
    --remote` or `cd recipe && git pull origin main`.

--- verl/experimental/agent_loop/agent_loop.py
+++ verl/experimental/agent_loop/agent_loop.py
@@ -286,7 +286,7 @@ class AgentLoopBase(ABC):
                 text=[raw_prompt],
                 images=images,
                 videos=videos,
-                video_metadatas=video_metadatas,
+                video_metadata=video_metadatas,
                 return_tensors="pt",
                 do_sample_frames=False,
             )
@@ -647,7 +647,7 @@ class AgentLoopWorker:
             text=[current_text],
             images=images,
             videos=videos,
-            video_metadatas=video_metadatas,
+            video_metadata=video_metadatas,
             return_tensors="pt",
             do_sample_frames=False,
         )
